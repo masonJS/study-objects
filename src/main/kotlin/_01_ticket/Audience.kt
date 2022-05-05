a3c7447b8b064a7d730ddc983c3226c0e6cf1766
minwoo <haruLife@inflearn.com>
2022-05-05 15:16:00 +0900

    리팩토링: 관람객을 자율적인 존재로 만들자.

--- src/main/kotlin/_01_ticket/Audience.kt
+++ src/main/kotlin/_01_ticket/Audience.kt
@@ -1,3 +1,12 @@
 package _01_ticket
 
-class Audience(val bag: Bag)
+class Audience(val bag: Bag) {
+  public fun buy(ticket: Ticket): Long = if (bag.hasInvitation()) {
+    bag.setTicket(ticket)
+    0;
+  } else {
+    bag.minusAmount(ticket.fee)
+    bag.setTicket(ticket)
+    ticket.fee;
+  }
+}
--- src/main/kotlin/_01_ticket/TicketSeller.kt
+++ src/main/kotlin/_01_ticket/TicketSeller.kt
@@ -2,14 +2,6 @@ package _01_ticket
 
 class TicketSeller(private val ticketOffice: TicketOffice) {
   public fun sellTo(audience: Audience) {
-    if (audience.bag.hasInvitation()) {
-      val ticket = ticketOffice.ticket
-      audience.bag.setTicket(ticket)
-    } else {
-      val ticket = ticketOffice.ticket
-      audience.bag.minusAmount(ticket.fee)
-      ticketOffice.plusAmount(ticket.fee)
-      audience.bag.setTicket(ticket)
-    }
+    ticketOffice.plusAmount(audience.buy(ticketOffice.ticket))
   }
 }
