646eb10ae832ad97bc627b4f5b318fc738c2b369
minwoo <haruLife@inflearn.com>
2022-05-05 15:08:48 +0900

    리팩토링: 판매원을 자율적인 존재로 만들자.

--- src/main/kotlin/_01_ticket/Theater.kt
+++ src/main/kotlin/_01_ticket/Theater.kt
@@ -2,14 +2,6 @@ package _01_ticket
 
 class Theater(private val ticketSeller: TicketSeller) {
   fun enter(audience: Audience) {
-    if (audience.bag.hasInvitation()) {
-      val ticket = ticketSeller.ticketOffice.ticket
-      audience.bag.setTicket(ticket)
-    } else {
-      val ticket = ticketSeller.ticketOffice.ticket
-      audience.bag.minusAmount(ticket.fee)
-      ticketSeller.ticketOffice.plusAmount(ticket.fee)
-      audience.bag.setTicket(ticket)
-    }
+    ticketSeller.sellTo(audience)
   }
 }
--- src/main/kotlin/_01_ticket/TicketSeller.kt
+++ src/main/kotlin/_01_ticket/TicketSeller.kt
@@ -1,3 +1,15 @@
 package _01_ticket
 
-class TicketSeller(val ticketOffice: TicketOffice)
+class TicketSeller(private val ticketOffice: TicketOffice) {
+  public fun sellTo(audience: Audience) {
+    if (audience.bag.hasInvitation()) {
+      val ticket = ticketOffice.ticket
+      audience.bag.setTicket(ticket)
+    } else {
+      val ticket = ticketOffice.ticket
+      audience.bag.minusAmount(ticket.fee)
+      ticketOffice.plusAmount(ticket.fee)
+      audience.bag.setTicket(ticket)
+    }
+  }
+}
